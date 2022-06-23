f327b0bfba25cd28af9cb8594257133e1bda412e
chigov2 <chigov@gmail.com>
2022-06-23 22:22:43 +0300

    126

--- src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
+++ src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
@@ -2,9 +2,13 @@ package com.chigov.jpabasic.repository;
 
 import com.chigov.jpabasic.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repos
+
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
+
+@RepositoryRe
 public interface CourseSpringDataRepository extends JpaRepository<Course,Long> {
     List<Course> findByName(String name);
     int countByName(String name);
