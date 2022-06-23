77c63b6ef7a54a8dac6148ed3bb29138fc3fa979
chigov2 <chigov@gmail.com>
2022-06-23 14:57:26 +0300

    124

--- src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
+++ src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
@@ -3,5 +3,9 @@ package com.chigov.jpabasic.repository;
 import com.chigov.jpabasic.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseSpringDataRepository extends JpaRepository<Course,Long> {
+    List<Course> findByName(String name);
+    int countByName(String name);
 }
--- test-junit/com/chigov/jpabasic/CourseRepositoryJpaTest.java
+++ test-junit/com/chigov/jpabasic/CourseRepositoryJpaTest.java
@@ -10,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,6 +40,23 @@ public class CourseRepositoryJpaTest {
         //logger.info("{testtest}",courseOptional.isPresent());
         assertTrue(courseOptional.isPresent());
     }
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0,3);
+        Page<Course> pageOne = repository.findAll(pageRequest);
+        logger.info("First 3 courses: {}", pageOne.getContent());
 
+        Pageable secondPage = pageOne.nextPageable();
+
+        Page<Course> secondPage1 = repository.findAll(secondPage);
+
+        logger.info("Next pageable element: {}", secondPage1.getContent());
+    }
+
+    @Test
+    public void findByName(){
+        logger.info("Find by name -> {}",repository.findByName("JPA in 50 steps"));
+        logger.info("Count by name -> {}",repository.countByName("JPA in 50 steps"));
+    }
 
 }
