e2b4cf9d8f2965a10255f877acfb6c4246da883d
Max2 <chigov@gmail.com>
2022-06-23 20:59:39 +0300

    eeee

--- src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
+++ src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
@@ -4,7 +4,7 @@ import com.chigov.jpabasic.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+@Repos
 public interface CourseSpringDataRepository extends JpaRepository<Course,Long> {
     List<Course> findByName(String name);
     int countByName(String name);
