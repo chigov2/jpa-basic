68c96d2ce1eb56cad4fbcf5ed8ccaf29972b2b29
Max2 <chigov@gmail.com>
2022-06-24 09:11:36 +0300

    REST

--- src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
+++ src/main/java/com/chigov/jpabasic/repository/CourseSpringDataRepository.java
@@ -2,9 +2,11 @@ package com.chigov.jpabasic.repository;
 
 import com.chigov.jpabasic.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repos
+@RepositoryRestResource(path="courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course,Long> {
     List<Course> findByName(String name);
     int countByName(String name);
